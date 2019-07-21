337d4b0428e7dabc2422396ef07b1dfebe09207d
Georgi Popov <magadanski_uchen@yahoo.com>
2019-07-21 12:49:57 +0300

    WIP on multikeyframe interaction
    1. move body height calculation in init method (re-execute on resize)
    2. add sample keyframes (base start and end values on windowTop, rater than full page height)
    3. build element styles in variable; calculate final styling and apply in a single call
    4. update icon styling
    5. add easing function
    
    TODO: figure out to-0 interpolation

--- functions.js
+++ functions.js
@@ -2,10 +2,26 @@ const keyframes = [
 	{
 		selector: '.fa-sun',
 		start: 0,
-		end: '200%',
+		end: '100%',
 		property: 'rotate',
 		from: 0,
 		to: 180
+	},
+	{
+		selector: '.fa-sun',
+		start: 0,
+		end: '20%',
+		property: 'opacity',
+		from: 0,
+		to: 1
+	},
+	{
+		selector: '.fa-sun',
+		start: '80%',
+		end: '100%',
+		property: 'opacity',
+		from: 1,
+		to: 0
 	}
 ];
 
@@ -13,7 +29,6 @@ let absoluteKeyframes = {};
 
 document.addEventListener('DOMContentLoaded', e => {
 	init();
-	document.body.style.height = Math.max.apply(Math, absoluteKeyframes.map(object => object.end)) + 'px';
 });
 
 window.addEventListener('resize', e => {
@@ -24,17 +39,31 @@ window.addEventListener('resize', e => {
 
 window.addEventListener('scroll', e => {
 	requestAnimationFrame(() => {
+		const animations = {};
+		
 		for (frame of absoluteKeyframes) {
-			const element = document.querySelector(frame.selector);
+			animations[frame.selector] = animations[frame.selector] || {};
 			
 			if (window.scrollY >= frame.start && window.scrollY <= frame.end) {
-				const scrollProgress = window.scrollY / (frame.end - window.innerHeight);
+				const value = -frame.to/2 * (Math.cos(Math.PI*window.scrollY/frame.end) - 1) + frame.from;
 				
 				switch (frame.property) {
 					case 'rotate':
-						element.style.transform = `rotate(${parseInt(frame.to * scrollProgress)}deg)`;
+						animations[frame.selector].transform = animations[frame.selector].transform || [];
+						animations[frame.selector].transform.push(`rotate(${value}deg)`);
+						break;
+					case 'opacity':
+						animations[frame.selector].opacity = (value).toFixed(2);
 						break;
 				}
+			}
+		}
+		
+		for (const selector in animations) {
+			const element = document.querySelector(selector);
+			
+			for (const property in animations[selector]) {
+				element.style[property] = (typeof(animations[selector][property]) == 'object') ? animations[selector][property].join(' ') : animations[selector][property];
 			}
 		}
 	});
@@ -42,6 +71,7 @@ window.addEventListener('scroll', e => {
 
 function init() {
 	absoluteKeyframes = relativeToAbsolute(keyframes);
+	document.body.style.height = Math.max.apply(Math, absoluteKeyframes.map(object => object.end + window.innerHeight)) + 'px';
 }
 
 function relativeToAbsolute(frames) {
